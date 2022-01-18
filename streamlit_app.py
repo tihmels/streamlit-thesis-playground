61f4f23ab5ba94e29f03f9961ec9beca7e50115a
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-01-18 17:50:06 +0100

    video name split

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ VIDEO_DIR = "videos"
 videos = glob.glob(VIDEO_DIR + "/*.mp4")
 
 add_selectbox = st.sidebar.selectbox(
-    "Select a video", videos.split("/", 1)[1], format_func = lambda label:label.split("/", 1)[1]
+    "Select a video", videos, format_func = lambda l:l.split("/", 1)[1]
 )
 
 st.title("TEST")
