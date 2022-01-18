d063edf665169c72735ec2dfda55e3bf60010200
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-01-18 17:36:03 +0100

    sidebar and videos selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import time
+import glob
+
+VIDEO_DIR = "videos"
+videos = glob.glob(VIDEO_DIR + "/*.mp4")
+
+add_selectbox = st.sidebar.selectbox(
+    "Select video to inspect", videos
+)
 
 st.title("TEST")
