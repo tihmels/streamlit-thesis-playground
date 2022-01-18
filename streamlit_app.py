777a86b784fc56f0e1591a38ab0bd6fce94c488f
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-01-18 17:57:51 +0100

    video player main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,16 @@ import glob
 VIDEO_DIR = "videos"
 videos = glob.glob(VIDEO_DIR + "/*.mp4")
 
+t = st.empty()
+
+def load_main_page(video):
+    video_file = open(video, 'rb')
+    video_bytes = video_file.read()
+
+    t.video(video_bytes)
+
 add_selectbox = st.sidebar.selectbox(
-    "Select a video", videos, format_func = lambda l:l.split("/", 1)[1]
+    "Select a video", videos, format_func = lambda l:l.split("/", 1)[1], on_change=load_main_page
 )
 
 st.title("TEST")
