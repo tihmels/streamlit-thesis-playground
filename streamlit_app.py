5ea054b9348a7287ed921c36fe422bad8601765a
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-01-19 11:41:47 +0100

    include test print

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,16 @@ videos = glob.glob(VIDEO_DIR + "/*.mp4")
 
 t = st.empty()
 
+
 def load_video(video):
+    print("called")
     video_file = open(video, 'rb')
     video_bytes = video_file.read()
 
     st.video(video_bytes)
 
-add_selectbox = st.sidebar.selectbox(
-    "Select a video", videos, format_func = lambda l:l.split("/", 1)[1], on_change=load_video)
+
+option = st.sidebar.selectbox(
+    "Select a video", videos, format_func=lambda l: l.split("/", 1)[1], on_change=load_video)
 
 st.title("TEST")
