9f681d704fbdd3416ba360b1768b8e29b7d8c0d9
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-01-26 17:36:10 +0100

    refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,19 +12,20 @@ st.set_page_config(
 )
 
 VIDEO_DIR = "videos"
-videos = glob.glob(VIDEO_DIR + "/*.mp4")
 
 
 def main():
-    selected_option = st.sidebar.selectbox("Select video", videos, format_func=lambda l: l.split("/", 1)[1])
+    videos = glob.glob(VIDEO_DIR + "/*.mp4")
+    selected_option = st.sidebar.selectbox("Select video for inspection", videos,
+                                           format_func=lambda l: l.split("/", 1)[1])
     load_video(selected_option)
 
 
 def load_video(video):
     st.title(video.split("/", 1)[1])
 
-    video_file = open(video, 'rb')
-    video_bytes = video_file.read()
+    video_stream = open(video, 'rb')
+    video_bytes = video_stream.read()
 
     st.video(video_bytes)
 
@@ -33,34 +34,35 @@ def load_video(video):
     if cap.isOpened():
         frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        fps = cap.get(cv2.CAP_PROP_FPS)
         frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        fps = cap.get(cv2.CAP_PROP_FPS)
         duration = frame_count / fps
+        minutes = int(duration / 60)
+        seconds = int(duration % 60)
+
+        video_range = st.slider(
+            "Select video range:",
+            value=(time(0, 0), time(0, minutes, seconds)),
+            min_value=(time(0, 0, 0)),
+            max_value=(time(0, minutes, seconds)),
+            step=timedelta(0, 1),
+            format="mm:ss"
+        )
 
         st.write("Width: " + str(frame_width))
         st.write("Height: " + str(frame_height))
         st.write("FPS: " + str(fps))
-        st.write("Frame count: " + str(frame_count))
-        st.write('Duration (S) = ' + str(duration))
-        minutes = int(duration / 60)
-        seconds = int(duration % 60)
-        st.write('Duration (M:S) = ' + str(minutes) + ':' + str(seconds))
+        st.write("Frames: " + str(frame_count))
+        st.write('Duration (s) = ' + str(duration))
 
-    appointment = st.slider(
-        "Select video range:",
-        value=(time(0, 0), time(0, minutes, seconds)),
-        min_value=(time(0, 0, 0)),
-        max_value=(time(0, minutes, seconds)),
-        step=timedelta(0, 1),
-        format="mm:ss"
-    )
+        st.write('Duration (m:s) = ' + str(minutes) + ':' + str(seconds))
 
-    # success, image = cap.read()
-    # count = 0
+        # success, image = cap.read()
+        # count = 0
 
-    # while success:
-    #  success, image = cap.read()
-    #  count += 1
+        # while success:
+        #  success, image = cap.read()
+        #  count += 1
 
 
 if __name__ == "__main__":
