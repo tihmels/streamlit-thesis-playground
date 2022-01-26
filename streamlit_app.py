e45aa271719ce0a46da39fed13341f2a8c50d078
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-01-26 17:26:45 +0100

    page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Thesis Playground App",
     layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    initial_sidebar_state="expanded"
 )
 
 VIDEO_DIR = "videos"
