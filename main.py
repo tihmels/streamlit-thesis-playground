a6b810d3f6f6e7dfffd726efcfa4e0fcb683fddc
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-02-04 16:08:10 +0100

    refactoring

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ def load_video(video):
 
     set_sidebar_info(vd)
 
-    # frames = extract_frames_from_video(cap)
+    frames = extract_frames_from_video(cap)
 
-    # st.write(frames.shape)
+    st.write(frames.shape)
 
     # video_range = st.slider(
     #    "Select video range:",
